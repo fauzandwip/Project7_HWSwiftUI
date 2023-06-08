e3a96ed357e237b576361a83ac53eca7db956cfe
fauzandp <fauzan27dp@gmail.com>
2023-06-08 10:57:38 +0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Fauzan Dwi Prasetyo on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
